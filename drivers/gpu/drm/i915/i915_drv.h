a066d13fe73380a608ba7bc94d5441ca86e6564f
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-03-02 17:22:13 +0200

    drm/i915: Store rawclk_freq in dev_priv
    Generalize rawclk handling by storing it in dev_priv.
    
    Presumably our hrawclk readout works at least for CTG and ELK
    since we've been using it for DP AUX on those platforms. There
    are no real docs anymore after configdb vanished, so the only
    reference is the public CTG GMCH spec. What bits are listed in
    that doc match our code. The ELK GMCH spec have no relevant
    details unfortunately.
    
    The PNV situation is less clear. Starting from
    commit aa17cdb4f836 ("drm/i915: initialize backlight max from VBT")
    we assume that the CTG/ELK hrawclk readout works for PNV as well.
    At least the results *seem* reasonable for one PNV machine (Lenovo
    Ideapad S10-3t). Sadly the PNV GMCH spec doesn't have the goods on
    the relevant register either.
    
    So let's keep assuming it works for PNV,ELK,CTG and read it out on
    those platforms. G33 also has hrawclk according to some notes
    in BSpec, but we don't actually need it for anything, so let's not
    even try to read it out there.
    
    v2: Rebase due to IS_VALLYVIEW vs. IS_CHERRYVIEW split
        Use KHz() all over, and kill off a few useless temp variables
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1456932138-14004-2-git-send-email-ville.syrjala@linux.intel.com
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1841,6 +1841,7 @@ struct drm_i915_private {
 	unsigned int skl_boot_cdclk;
 	unsigned int cdclk_freq, max_cdclk_freq, atomic_cdclk_freq;
 	unsigned int max_dotclk_freq;
+	unsigned int rawclk_freq;
 	unsigned int hpll_freq;
 	unsigned int czclk_freq;
 
