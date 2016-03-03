9617d803e803640cef07dbddee0ae2bce7c723d8
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2016-03-03 14:36:41 +0000

    drm/i915: Add wait_for_us
    This is for callers who want micro-second precision but are not
    waiting from the atomic context.
    
    v2:
      * Fix atomic waits. (Dave Gordon)
      * Use USEC_PER_SEC and USEC_PER_MSEC. (Chris Wilson)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Dave Gordon <david.s.gordon@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -67,10 +67,11 @@
 	ret__;								\
 })
 
-#define wait_for(COND, MS) _wait_for(COND, MS, 1)
-#define wait_for_atomic(COND, MS) _wait_for(COND, MS, 0)
-#define wait_for_atomic_us(COND, US) _wait_for((COND), \
-					       DIV_ROUND_UP((US), 1000), 0)
+#define wait_for(COND, MS)	  	_wait_for((COND), (MS) * 1000, 1000)
+#define wait_for_us(COND, US)	  	_wait_for((COND), (US), 1)
+
+#define wait_for_atomic(COND, MS) 	_wait_for((COND), (MS) * 1000, 0)
+#define wait_for_atomic_us(COND, US)	_wait_for((COND), (US), 0)
 
 #define KHz(x) (1000 * (x))
 #define MHz(x) KHz(1000 * (x))
