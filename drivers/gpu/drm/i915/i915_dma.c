37c48d8c2a2c219d11ffc47bb328219b43f8fd27
Imre Deak <imre.deak@intel.com>
2016-03-16 13:38:59 +0200

    drm/i915: Move load time audio component registration earlier
    We should register all the interfaces before we enable runtime PM.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1458128348-15730-11-git-send-email-imre.deak@intel.com

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1167,9 +1167,9 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	if (IS_GEN5(dev))
 		intel_gpu_ips_init(dev_priv);
 
-	intel_runtime_pm_enable(dev_priv);
-
 	i915_audio_component_init(dev_priv);
+
+	intel_runtime_pm_enable(dev_priv);
 
 	intel_runtime_pm_put(dev_priv);
 
@@ -1210,8 +1210,6 @@ int i915_driver_unload(struct drm_device *dev)
 
 	intel_fbdev_fini(dev);
 
-	i915_audio_component_cleanup(dev_priv);
-
 	ret = i915_gem_suspend(dev);
 	if (ret) {
 		DRM_ERROR("failed to idle hardware: %d\n", ret);
@@ -1219,6 +1217,8 @@ int i915_driver_unload(struct drm_device *dev)
 	}
 
 	intel_power_domains_fini(dev_priv);
+
+	i915_audio_component_cleanup(dev_priv);
 
 	intel_gpu_ips_teardown();
 
