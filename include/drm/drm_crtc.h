1c36a5c835ed574d55c43188bd3984eb3bfb4105
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2016-03-23 11:42:54 +0300

    drm: Rename drm_connector_unplug_all() to drm_connector_unregister_all()
    Current name is a bit misleading because what that helper function
    really does it calls drm_connector_unregister() for all connectors.
    
    This all has nothing to do with hotplugging so let's name things
    properly.
    
    And while at it remove potentially dangerous locking around
    drm_connector_unregister() in rcar_du_remove() as mentioned
    in kerneldoc for drm_connector_unregister_all().
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Boris Brezillon <boris.brezillon@free-electrons.com>
    Cc: linux-renesas-soc@vger.kernel.org
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1458722577-20283-2-git-send-email-abrodkin@synopsys.com

--- include/drm/drm_crtc.h
+++ include/drm/drm_crtc.h
@@ -2254,13 +2254,9 @@ int drm_connector_register(struct drm_connector *connector);
 void drm_connector_unregister(struct drm_connector *connector);
 
 extern void drm_connector_cleanup(struct drm_connector *connector);
-static inline unsigned drm_connector_index(struct drm_connector *connector)
-{
-	return connector->connector_id;
-}
-
-/* helper to unplug all connectors from sysfs for device */
-extern void drm_connector_unplug_all(struct drm_device *dev);
+extern unsigned int drm_connector_index(struct drm_connector *connector);
+/* helper to unregister all connectors from sysfs for device */
+extern void drm_connector_unregister_all(struct drm_device *dev);
 
 extern int drm_bridge_add(struct drm_bridge *bridge);
 extern void drm_bridge_remove(struct drm_bridge *bridge);
