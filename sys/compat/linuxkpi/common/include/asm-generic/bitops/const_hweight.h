05f04ae5eb9153aed12f8d7403b61935afa9c29c
Matthew Macy <mmacy@nextbsd.org>
2016-03-31 17:28:23 -0700

    more compat bits

---
+++ sys/compat/linuxkpi/common/include/asm-generic/bitops/const_hweight.h
@@ -0,0 +1,8 @@
+#ifndef _ASM_GENERIC_BITOPS_CONST_HWEIGHT_H_
+#define _ASM_GENERIC_BITOPS_CONST_HWEIGHT_H_
+
+#define hweight8(x) bitcount8(x)
+#define hweight16(x) bitcount16(x)
+#define hweight32(x) bitcount32(x)
+
+#endif /* _ASM_GENERIC_BITOPS_CONST_HWEIGHT_H_ */
