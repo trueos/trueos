0acfe48ae8d748d2b230916df9ded35e234793f4
Matthew Macy <mmacy@nextbsd.org>
2016-04-18 16:58:29 -0700

    factor out tainted fb functionality in to a separate file

--- sys/dev/drm2/linux_fb.c
+++ sys/dev/drm2/linux_fb.c
@@ -11,95 +11,387 @@ __FBSDID("$FreeBSD$");
 #include <sys/param.h>
 #include <sys/systm.h>
 
-static u16 red2[] __read_mostly = {
-    0x0000, 0xaaaa
-};
-static u16 green2[] __read_mostly = {
-    0x0000, 0xaaaa
-};
-static u16 blue2[] __read_mostly = {
-    0x0000, 0xaaaa
-};
+#define FB_MAJOR		29   /* /dev/fb* framebuffers */
+#define FBPIXMAPSIZE	(1024 * 8)
 
-static u16 red4[] __read_mostly = {
-    0x0000, 0xaaaa, 0x5555, 0xffff
-};
-static u16 green4[] __read_mostly = {
-    0x0000, 0xaaaa, 0x5555, 0xffff
-};
-static u16 blue4[] __read_mostly = {
-    0x0000, 0xaaaa, 0x5555, 0xffff
-};
 
-static u16 red8[] __read_mostly = {
-    0x0000, 0x0000, 0x0000, 0x0000, 0xaaaa, 0xaaaa, 0xaaaa, 0xaaaa
-};
-static u16 green8[] __read_mostly = {
-    0x0000, 0x0000, 0xaaaa, 0xaaaa, 0x0000, 0x0000, 0x5555, 0xaaaa
-};
-static u16 blue8[] __read_mostly = {
-    0x0000, 0xaaaa, 0x0000, 0xaaaa, 0x0000, 0xaaaa, 0x0000, 0xaaaa
-};
+static struct mtx linux_fb_mtx;
+MTX_SYSINIT(linux_fb_mtx, &linux_fb_mtx, "linux fb", MTX_DEF);
 
-static u16 red16[] __read_mostly = {
-    0x0000, 0x0000, 0x0000, 0x0000, 0xaaaa, 0xaaaa, 0xaaaa, 0xaaaa,
-    0x5555, 0x5555, 0x5555, 0x5555, 0xffff, 0xffff, 0xffff, 0xffff
-};
-static u16 green16[] __read_mostly = {
-    0x0000, 0x0000, 0xaaaa, 0xaaaa, 0x0000, 0x0000, 0x5555, 0xaaaa,
-    0x5555, 0x5555, 0xffff, 0xffff, 0x5555, 0x5555, 0xffff, 0xffff
-};
-static u16 blue16[] __read_mostly = {
-    0x0000, 0xaaaa, 0x0000, 0xaaaa, 0x0000, 0xaaaa, 0x0000, 0xaaaa,
-    0x5555, 0xffff, 0x5555, 0xffff, 0x5555, 0xffff, 0x5555, 0xffff
-};
+static struct linux_fb_info *registered_fb[FB_MAX];
+static int num_registered_fb;
+static struct class *fb_class;
 
-static const struct fb_cmap default_2_colors = {
-    .len=2, .red=red2, .green=green2, .blue=blue2
-};
-static const struct fb_cmap default_8_colors = {
-    .len=8, .red=red8, .green=green8, .blue=blue8
-};
-static const struct fb_cmap default_4_colors = {
-    .len=4, .red=red4, .green=green4, .blue=blue4
-};
-static const struct fb_cmap default_16_colors = {
-    .len=16, .red=red16, .green=green16, .blue=blue16
-};
+static int __unregister_framebuffer(struct linux_fb_info *fb_info);
 
 struct linux_fb_info *
 linux_framebuffer_alloc(size_t size, struct device *dev)
 {
-	return (NULL);
+#define BYTES_PER_LONG (BITS_PER_LONG/8)
+#define PADDING (BYTES_PER_LONG - (sizeof(struct linux_fb_info) % BYTES_PER_LONG))
+	int fb_info_size = sizeof(struct linux_fb_info);
+	struct linux_fb_info *info;
+	char *p;
+
+	if (size)
+		fb_info_size += PADDING;
+
+	p = kzalloc(fb_info_size + size, GFP_KERNEL);
+
+	if (!p)
+		return NULL;
+
+	info = (struct linux_fb_info *) p;
+
+	if (size)
+		info->par = p + fb_info_size;
+
+	info->device = dev;
+
+	return info;
+#undef PADDING
+#undef BYTES_PER_LONG
 }
+
 
 void
 linux_framebuffer_release(struct linux_fb_info *info)
 {
-	panic("XXX");
+	if (info == NULL)
+		return;
+	kfree(info->apertures);
+	kfree(info);
+}
+
+static void
+put_fb_info(struct linux_fb_info *fb_info)
+{
+	if (!atomic_dec_and_test(&fb_info->count))
+		return;
+	if (fb_info->fbops->fb_destroy)
+		fb_info->fbops->fb_destroy(fb_info);
+}
+
+static bool
+apertures_overlap(struct aperture *gen, struct aperture *hw)
+{
+	/* is the generic aperture base the same as the HW one */
+	if (gen->base == hw->base)
+		return (true);
+	/* is the generic aperture base inside the hw base->hw base+size */
+	if (gen->base > hw->base && gen->base < hw->base + hw->size)
+		return (true);
+	return (false);
+}
+
+static bool
+check_overlap(struct apertures_struct *a, struct apertures_struct *b)
+{
+	int i, j;
+
+	if (a == NULL || b == NULL)
+		return (false);
+
+	for (i = 0; i < b->count; ++i)
+		for (j = 0; j < a->count; ++j) 
+			if (apertures_overlap(&a->ranges[j], &b->ranges[i]))
+				return (true);
+	return (false);
 }
 
 
+
+#define VGA_FB_PHYS 0xA0000
+static void
+__remove_conflicting(struct apertures_struct *a, const char *name, bool primary)
+{
+	struct apertures_struct *gen_aper;
+	int i;
+
+	for (i = 0 ; i < FB_MAX; i++) {
+
+		if (registered_fb[i] == NULL)
+			continue;
+
+		if ((registered_fb[i]->flags & FBINFO_MISC_FIRMWARE) == 0)
+			continue;
+
+		gen_aper = registered_fb[i]->apertures;
+		if (check_overlap(gen_aper, a) ||
+			(primary && gen_aper && gen_aper->count &&
+			 gen_aper->ranges[0].base == VGA_FB_PHYS)) {
+			__unregister_framebuffer(registered_fb[i]);
+		}
+	}
+}
+
 void
-remove_conflicting_framebuffers(struct apertures_struct *a,
+linux_remove_conflicting_framebuffers(struct apertures_struct *a,
 				const char *name, bool primary)
 {
-	panic("XXX");
+	mtx_lock(&linux_fb_mtx);
+	__remove_conflicting(a, name, primary);
+	mtx_unlock(&linux_fb_mtx);
+}
+
+static int
+is_primary(struct linux_fb_info *info)
+{
+	struct device *device = info->device;
+	struct pci_dev *pci_dev = NULL;
+	struct linux_resource *res = NULL;
+
+	if (device &&  (pci_dev = to_pci_dev(device)) == NULL)
+		return (0);
+
+	res = &pci_dev->resource[PCI_ROM_RESOURCE];
+	if (res && res->flags & IORESOURCE_ROM_SHADOW)
+		return (1);
+
+	return (0);
+}
+
+
+static int
+fb_mode_is_equal(const struct fb_videomode *mode1,
+		     const struct fb_videomode *mode2)
+{
+	return (mode1->xres         == mode2->xres &&
+		mode1->yres         == mode2->yres &&
+		mode1->pixclock     == mode2->pixclock &&
+		mode1->hsync_len    == mode2->hsync_len &&
+		mode1->vsync_len    == mode2->vsync_len &&
+		mode1->left_margin  == mode2->left_margin &&
+		mode1->right_margin == mode2->right_margin &&
+		mode1->upper_margin == mode2->upper_margin &&
+		mode1->lower_margin == mode2->lower_margin &&
+		mode1->sync         == mode2->sync &&
+		mode1->vmode        == mode2->vmode);
+}
+static void
+fb_destroy_modes(struct list_head *head)
+{
+	struct list_head *pos, *n;
+
+	list_for_each_safe(pos, n, head) {
+		list_del(pos);
+		kfree(pos);
+	}
+}
+
+
+static int
+fb_add_videomode(const struct fb_videomode *mode, struct list_head *head)
+{
+	struct list_head *pos;
+	struct fb_modelist *modelist;
+	struct fb_videomode *m;
+	int found = 0;
+
+	list_for_each(pos, head) {
+		modelist = list_entry(pos, struct fb_modelist, list);
+		m = &modelist->mode;
+		if (fb_mode_is_equal(m, mode)) {
+			found = 1;
+			break;
+		}
+	}
+	if (!found) {
+		modelist = kmalloc(sizeof(struct fb_modelist),
+						  GFP_KERNEL);
+
+		if (!modelist)
+			return -ENOMEM;
+		modelist->mode = *mode;
+		list_add(&modelist->list, head);
+	}
+	return 0;
+}
+
+static void
+fb_var_to_videomode(struct fb_videomode *mode,
+			 const struct fb_var_screeninfo *var)
+{
+	u32 pixclock, hfreq, htotal, vtotal;
+
+	mode->name = NULL;
+	mode->xres = var->xres;
+	mode->yres = var->yres;
+	mode->pixclock = var->pixclock;
+	mode->hsync_len = var->hsync_len;
+	mode->vsync_len = var->vsync_len;
+	mode->left_margin = var->left_margin;
+	mode->right_margin = var->right_margin;
+	mode->upper_margin = var->upper_margin;
+	mode->lower_margin = var->lower_margin;
+	mode->sync = var->sync;
+	mode->vmode = var->vmode & FB_VMODE_MASK;
+	mode->flag = FB_MODE_IS_FROM_VAR;
+	mode->refresh = 0;
+
+	if (!var->pixclock)
+		return;
+
+	pixclock = PICOS2KHZ(var->pixclock) * 1000;
+
+	htotal = var->xres + var->right_margin + var->hsync_len +
+		var->left_margin;
+	vtotal = var->yres + var->lower_margin + var->vsync_len +
+		var->upper_margin;
+
+	if (var->vmode & FB_VMODE_INTERLACED)
+		vtotal /= 2;
+	if (var->vmode & FB_VMODE_DOUBLE)
+		vtotal *= 2;
+
+	hfreq = pixclock/htotal;
+	mode->refresh = hfreq/vtotal;
+}
+
+static int
+__register_framebuffer(struct linux_fb_info *fb_info)
+{
+	int i;
+	struct fb_event event;
+	struct fb_videomode mode;
+
+	__remove_conflicting(fb_info->apertures, fb_info->fix.id, is_primary(fb_info));
+
+	if (num_registered_fb == FB_MAX)
+		return -ENXIO;
+
+	num_registered_fb++;
+	for (i = 0 ; i < FB_MAX; i++)
+		if (!registered_fb[i])
+			break;
+	fb_info->node = i;
+	atomic_set(&fb_info->count, 1);
+	mutex_init(&fb_info->lock);
+	mutex_init(&fb_info->mm_lock);
+
+	fb_info->dev = device_create(fb_class, fb_info->device,
+				     MKDEV(FB_MAJOR, i), NULL, "fb%d", i);
+	if (IS_ERR(fb_info->dev)) {
+		/* Not fatal */
+		printk(KERN_WARNING "Unable to create device for framebuffer %d; errno = %ld\n", i, PTR_ERR(fb_info->dev));
+		fb_info->dev = NULL;
+	} else
+		dev_set_drvdata(fb_info->dev, fb_info);
+
+	if (fb_info->pixmap.addr == NULL) {
+		fb_info->pixmap.addr = kmalloc(FBPIXMAPSIZE, GFP_KERNEL);
+		if (fb_info->pixmap.addr) {
+			fb_info->pixmap.size = FBPIXMAPSIZE;
+			fb_info->pixmap.buf_align = 1;
+			fb_info->pixmap.scan_align = 1;
+			fb_info->pixmap.access_align = 32;
+			fb_info->pixmap.flags = FB_PIXMAP_DEFAULT;
+		}
+	}	
+	fb_info->pixmap.offset = 0;
+
+	if (!fb_info->pixmap.blit_x)
+		fb_info->pixmap.blit_x = ~(u32)0;
+
+	if (!fb_info->pixmap.blit_y)
+		fb_info->pixmap.blit_y = ~(u32)0;
+
+	if (!fb_info->modelist.prev || !fb_info->modelist.next)
+		INIT_LIST_HEAD(&fb_info->modelist);
+
+	fb_var_to_videomode(&mode, &fb_info->var);
+	fb_add_videomode(&mode, &fb_info->modelist);
+	registered_fb[i] = fb_info;
+
+	event.info = fb_info;
+#if 0	
+	if (!lock_fb_info(fb_info))
+		return -ENODEV;
+	console_lock();
+	fb_notifier_call_chain(FB_EVENT_FB_REGISTERED, &event);
+	console_unlock();
+	unlock_fb_info(fb_info);
+#endif	
+	return 0;
 }
 
 int
 linux_register_framebuffer(struct linux_fb_info *fb_info)
 {
+	int rc;
 
-	panic("XXX");
-	return (0);
+	mtx_lock(&linux_fb_mtx);
+	rc = __register_framebuffer(fb_info);
+	mtx_unlock(&linux_fb_mtx);
+	return (rc);
 }
+
+int
+unlink_framebuffer(struct linux_fb_info *fb_info)
+{
+	int i;
+
+	i = fb_info->node;
+	if (i < 0 || i >= FB_MAX || registered_fb[i] != fb_info)
+		return -EINVAL;
+
+	if (fb_info->dev) {
+		device_destroy(fb_class, MKDEV(FB_MAJOR, i));
+		fb_info->dev = NULL;
+	}
+	return 0;
+}
+
+static int
+__unregister_framebuffer(struct linux_fb_info *fb_info)
+{
+	struct fb_event event;
+	int i, ret = 0;
+
+	i = fb_info->node;
+	if (i < 0 || i >= FB_MAX || registered_fb[i] != fb_info)
+		return -EINVAL;
+
+#if 0	
+	if (!lock_fb_info(fb_info))
+		return -ENODEV;
+	console_lock();
+	event.info = fb_info;
+	ret = fb_notifier_call_chain(FB_EVENT_FB_UNBIND, &event);
+	console_unlock();
+	unlock_fb_info(fb_info);
+
+	if (ret)
+		return -EINVAL;
+#endif
+	
+	unlink_framebuffer(fb_info);
+	if (fb_info->pixmap.addr &&
+	    (fb_info->pixmap.flags & FB_PIXMAP_DEFAULT))
+		kfree(fb_info->pixmap.addr);
+	fb_destroy_modes(&fb_info->modelist);
+	registered_fb[i] = NULL;
+	num_registered_fb--;
+	event.info = fb_info;
+#if 0	
+	console_lock();
+	fb_notifier_call_chain(FB_EVENT_FB_UNREGISTERED, &event);
+	console_unlock();
+#endif
+	/* this may free fb info */
+	put_fb_info(fb_info);
+	return 0;
+}
+
 int
 linux_unregister_framebuffer(struct linux_fb_info *fb_info)
 {
+	int rc;
 
-	panic("XXX");
-	return (0);
+	mtx_lock(&linux_fb_mtx);
+	rc = __unregister_framebuffer(fb_info);
+	mtx_unlock(&linux_fb_mtx);
+	return (rc);
 }
 
 void
@@ -123,32 +415,20 @@ fb_set_suspend(struct linux_fb_info *info, int state)
 void
 cfb_fillrect(struct linux_fb_info *p, const struct fb_fillrect *rect)
 {
-	panic("XXX");
+	tainted_cfb_fillrect(p, rect);
 }
 
 void
 cfb_copyarea(struct linux_fb_info *p, const struct fb_copyarea *area)
 {
 
-	panic("XXX");
+	tainted_cfb_copyarea(p, area);
 }
 
 void
 cfb_imageblit(struct linux_fb_info *p, const struct fb_image *image)
 {
-	panic("XXX");
-}
-
-static const struct fb_cmap *
-fb_default_cmap(int len)
-{
-    if (len <= 2)
-	return &default_2_colors;
-    if (len <= 4)
-	return &default_4_colors;
-    if (len <= 8)
-	return &default_8_colors;
-    return &default_16_colors;
+	tainted_cfb_imageblit(p, image);
 }
 
 static int
@@ -206,7 +486,7 @@ fb_alloc_cmap_gfp(struct fb_cmap *cmap, int len, int transp, gfp_t flags)
 	}
 	cmap->start = 0;
 	cmap->len = len;
-	ret = fb_copy_cmap(fb_default_cmap(len), cmap);
+	ret = fb_copy_cmap(tainted_fb_default_cmap(len), cmap);
 	if (ret)
 		goto fail;
 	return 0;
