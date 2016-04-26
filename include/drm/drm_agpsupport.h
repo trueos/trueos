fdfba938b06759e6fd4f0b1888c8341c5722bb3b
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-04-26 19:29:34 +0200

    drm: Give drm_agp_clear drm_legacy_ prefix
    It has a  DRIVER_MODESET check to sure make it's not creating havoc
    for drm drivers. Make that clear in the name too.
    
    v2: Move misplaced hunk, spotted by 0day and Thierry.
    
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1461691808-12414-2-git-send-email-daniel.vetter@ffwll.ch

--- include/drm/drm_agpsupport.h
+++ include/drm/drm_agpsupport.h
@@ -40,7 +40,7 @@ DRM_AGP_MEM *drm_agp_bind_pages(struct drm_device *dev,
 				uint32_t type);
 
 struct drm_agp_head *drm_agp_init(struct drm_device *dev);
-void drm_agp_clear(struct drm_device *dev);
+void drm_legacy_agp_clear(struct drm_device *dev);
 int drm_agp_acquire(struct drm_device *dev);
 int drm_agp_acquire_ioctl(struct drm_device *dev, void *data,
 			  struct drm_file *file_priv);
@@ -96,7 +96,7 @@ static inline struct drm_agp_head *drm_agp_init(struct drm_device *dev)
 	return NULL;
 }
 
-static inline void drm_agp_clear(struct drm_device *dev)
+static inline void drm_legacy_agp_clear(struct drm_device *dev)
 {
 }
 
