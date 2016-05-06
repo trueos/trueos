d874d84b85a49fcc66331bc1672b4ded8aab4d46
Matt Macy <mmacy@nextbsd.org>
2016-05-05 21:41:38 -0700

    add fwnode to device_node and headers so that everything gets declared

--- sys/compat/linuxkpi/common/include/linux/of.h
+++ sys/compat/linuxkpi/common/include/linux/of.h
@@ -1,9 +1,22 @@
 #ifndef _LINUX_OF_H
 #define _LINUX_OF_H
 
+
+#include <linux/types.h>
+#include <linux/bitops.h>
+#include <linux/errno.h>
+#include <linux/kobject.h>
+#include <linux/mod_devicetable.h>
+#include <linux/spinlock.h>
+#include <linux/notifier.h>
+#include <linux/property.h>
+#include <linux/list.h>
+
 struct device_node {
 	const char *name;
 	const char *type;
+	struct fwnode_handle fwnode;
+
 };
 
 static inline struct device_node *of_node_get(struct device_node *node)
