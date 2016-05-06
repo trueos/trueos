3dbb55a4e34b297cc1f1540790367f14f9bc20bf
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-05-06 14:55:02 +0200

    drm: Fix error handling in drm_connector_register
    When debugfs or sysfs registration failed, we failed to clean up the
    idr registration. Reorder to fix this.
    
    Cc: Dave Airlie <airlied@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1462539302-27764-1-git-send-email-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -1027,8 +1027,6 @@ int drm_connector_register(struct drm_connector *connector)
 {
 	int ret;
 
-	drm_mode_object_register(connector->dev, &connector->base);
-
 	ret = drm_sysfs_connector_add(connector);
 	if (ret)
 		return ret;
@@ -1038,6 +1036,8 @@ int drm_connector_register(struct drm_connector *connector)
 		drm_sysfs_connector_remove(connector);
 		return ret;
 	}
+
+	drm_mode_object_register(connector->dev, &connector->base);
 
 	return 0;
 }
