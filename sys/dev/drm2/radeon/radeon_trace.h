b0be4f20be8a33b0bc073786e47be82b8a6a0f8c
J M Dieterich <dieterich@ogolem.org>
2016-05-05 22:32:29 -0400

    First round of porting done. Document what is missing and why.

--- sys/dev/drm2/radeon/radeon_trace.h
+++ sys/dev/drm2/radeon/radeon_trace.h
@@ -8,4 +8,16 @@ trace_radeon_cs(struct radeon_cs_parser * parser){
 	CTR1(KTR_DRM, "radeon_cs %p", parser);
 }
 
+static inline void
+trace_radeon_semaphore_signale(int ridx, struct radeon_semaphore * semaphore){
+	CTR2(KTR_DRM, "radeon_semaphore_signale %d %p", ridx, semaphore);
+}
+
+static inline void
+trace_radeon_semaphore_wait(int ridx, struct radeon_semaphore * semaphore){
+	CTR2(KTR_DRM, "radeon_semaphore_wait %d %p", ridx, semaphore);
+}
+
+
+
 #endif
