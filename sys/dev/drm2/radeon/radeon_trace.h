a6f9b48f03e2533c0f76670d36c29388a71af821
J M Dieterich <dieterich@ogolem.org>
2016-05-06 23:01:49 -0400

    Add some trace stuff.

--- sys/dev/drm2/radeon/radeon_trace.h
+++ sys/dev/drm2/radeon/radeon_trace.h
@@ -39,4 +39,25 @@ trace_radeon_fence_wait_end(void* ddev, int i, int seq){
 	CTR3(KTR_DRM, "radeon_fence_wait_end %p %d %d", ddev, i, seq);
 }
 
+static inline void
+trace_radeon_vm_grab_id(int i, int ring){
+	CTR2(KTR_DRM, "radeon_vm_grab_idi %d %d", i, ring);
+}
+
+static inline void
+trace_radeon_vm_flush(uint64_t pd_addr, int ring, int id){
+	CTR3(KTR_DRM, "radeon_vm_flush %x %d %d", pd_addr, ring, id);
+}
+
+static inline void
+trace_radeon_vm_set_page(uint64_t pe, uint64_t addr, unsigned count, uint32_t incr, uint32_t flags){
+	CTR5(KTR_DRM, "radeon_vm_set_page %x %x %d %d %d", pe, addr, count, incr, flags);
+	
+}
+
+static inline void
+trace_radeon_vm_bo_update(void* bo_va){
+	CTR1(KTR_DRM, "radeon_vm_bo_update %p", bo_va);
+}
+
 #endif
