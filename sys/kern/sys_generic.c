3b3d79b861896ab68729c1b0a8e9f5647debae76
Matt Macy <mmacy@nextbsd.org>
2016-05-09 12:19:08 -0700

    pass user address down for linux ioctl code to use skip copyout if cleared

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -739,10 +739,14 @@ sys_ioctl(struct thread *td, struct ioctl_args *uap)
 		 */
 		bzero(data, size);
 	}
-
+	/*
+	 * store the original user address in case a lower layer needs it
+	 * as in the case of the linuxkpi
+	 */
+	td->td_retval[1] = (register_t)uap->data;
 	error = kern_ioctl(td, uap->fd, com, data);
 
-	if (error == 0 && (com & IOC_OUT))
+	if (error == 0 && (com & IOC_OUT) && td->td_retval[1] != 0)
 		error = copyout(data, uap->data, (u_int)size);
 
 out:
