46690af3ce08352dd566f7b0e73b193618e66b2c
J M Dieterich <dieterich@ogolem.org>
2016-05-10 01:44:35 -0400

    Make sure amdkfd compiles. Does not link yet.

--- sys/compat/linuxkpi/common/include/linux/kfifo.h
+++ sys/compat/linuxkpi/common/include/linux/kfifo.h
@@ -4,4 +4,10 @@
 struct kfifo {
 };
 
+static inline int
+kfifo_alloc(struct kfifo *fifo, unsigned int size, size_t esize, gfp_t gfp_mask){
+	UNIMPLEMENTED();
+	return (0);
+}
+
 #endif
---
+++ sys/compat/linuxkpi/common/include/linux/pfn.h
@@ -0,0 +1,8 @@
+#ifndef _LINUX_PFN_H_
+#define _LINUX_PFN_H_
+
+typedef struct {
+        u64 val;
+} pfn_t;
+
+#endif
