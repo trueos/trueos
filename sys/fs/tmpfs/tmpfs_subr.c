47d629c6d9ae9e55ad75871dfbd8a72a792e50d4
Matt Macy <mmacy@nextbsd.org>
2016-05-28 17:38:49 -0700

    add extra arg to vm_pager_get_pages

--- sys/fs/tmpfs/tmpfs_subr.c
+++ sys/fs/tmpfs/tmpfs_subr.c
@@ -1371,7 +1371,7 @@ retry:
 					goto retry;
 				} else if (m->valid != VM_PAGE_BITS_ALL)
 					rv = vm_pager_get_pages(uobj, &m, 1,
-					    NULL, NULL);
+					    NULL, NULL, VM_PROT_READ|VM_PROT_WRITE);
 				else
 					/* A cached page was reactivated. */
 					rv = VM_PAGER_OK;
