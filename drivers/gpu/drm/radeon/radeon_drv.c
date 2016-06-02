8a052aec1bc9b393c828a531ac44b901486ce634
Alex Deucher <alexander.deucher@amd.com>
2016-06-02 09:31:59 -0400

    drm/radeon: drop explicit pci D3/D0 setting for ATPX power control
    The ATPX power control method does this for you.
    
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_drv.c
+++ drivers/gpu/drm/radeon/radeon_drv.c
@@ -43,6 +43,9 @@
 #include "drm_crtc_helper.h"
 #include "radeon_kfd.h"
 
+#define pci_restore_state linux_pci_restore_state
+#define pci_save_state linux_pci_save_state
+
 /*
  * KMS wrapper.
  * - 2.0.0 - initial interface
@@ -406,14 +409,12 @@ static int radeon_pmops_runtime_suspend(struct device *dev)
 	vga_switcheroo_set_dynamic_switch(pdev, VGA_SWITCHEROO_OFF);
 
 	ret = radeon_suspend_kms(drm_dev, false, false, false);
-	linux_pci_save_state(pdev);
+	pci_save_state(pdev);
 	pci_disable_device(pdev);
 	pci_ignore_hotplug(pdev);
 	if (radeon_is_atpx_hybrid())
 		pci_set_power_state(pdev, PCI_D3cold);
-	else if (radeon_has_atpx_dgpu_power_cntl())
-		pci_set_power_state(pdev, PCI_D3cold);
-	else
+	else if (!radeon_has_atpx_dgpu_power_cntl())
 		pci_set_power_state(pdev, PCI_D3hot);
 	drm_dev->switch_power_state = DRM_SWITCH_POWER_DYNAMIC_OFF;
 
@@ -431,8 +432,10 @@ static int radeon_pmops_runtime_resume(struct device *dev)
 
 	drm_dev->switch_power_state = DRM_SWITCH_POWER_CHANGING;
 
-	pci_set_power_state(pdev, PCI_D0);
-	linux_pci_restore_state(pdev);
+	if (radeon_is_atpx_hybrid() ||
+	    !radeon_has_atpx_dgpu_power_cntl())
+		pci_set_power_state(pdev, PCI_D0);
+	pci_restore_state(pdev);
 	ret = pci_enable_device(pdev);
 	if (ret)
 		return ret;
