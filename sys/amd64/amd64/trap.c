54de2641bb325a4ba1fe9a73bcf99f6ed18e4cea
Matt Macy <mmacy@nextbsd.org>
2016-06-02 14:17:49 -0700

    make sure stopsched is set

--- sys/amd64/amd64/trap.c
+++ sys/amd64/amd64/trap.c
@@ -832,6 +832,7 @@ trap_fatal(frame, eva)
 	    curproc->p_pid, curthread->td_name);
 
 #ifdef KDB
+	curthread->td_stopsched = 1;
 	if (debugger_on_panic || kdb_active)
 		if (kdb_trap(type, 0, frame))
 			return;
