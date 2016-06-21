75ab77485ba09652f5f487406edfcf449e70df4f
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-06-21 11:10:26 +0200

    drm/amdkfd: Clean up inline handling
    - inline functions need to be static inline, otherwise gcc can opt to
      not inline and the linker gets unhappy.
    - no forward decls for inline functions, just include the right headers.
    
    Cc: Oded Gabbay <oded.gabbay@gmail.com>
    Cc: Ben Goz <ben.goz@amd.com>
    Reviewed-by: Oded Gabbay <oded.gabbay@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1466500235-21282-2-git-send-email-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.h
@@ -187,7 +187,7 @@ int init_pipelines(struct device_queue_manager *dqm,
 unsigned int get_first_pipe(struct device_queue_manager *dqm);
 unsigned int get_pipes_num(struct device_queue_manager *dqm);
 
-inline static unsigned int get_sh_mem_bases_32(struct kfd_process_device *pdd)
+static inline unsigned int get_sh_mem_bases_32(struct kfd_process_device *pdd)
 {
 	return (pdd->lds_base >> 16) & 0xFF;
 }
