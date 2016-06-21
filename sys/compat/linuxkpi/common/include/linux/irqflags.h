4059f9b403724c1139f759571a0727e7a0696d35
Matt Macy <mmacy@nextbsd.org>
2016-06-21 02:50:31 -0700

    use critical sections for local_irq functions

--- sys/compat/linuxkpi/common/include/linux/irqflags.h
+++ sys/compat/linuxkpi/common/include/linux/irqflags.h
@@ -5,13 +5,13 @@
 static inline void
 local_irq_disable(void)
 {
-	spinlock_enter();
+	critical_enter();
 }
 
 static inline void
 local_irq_enable(void)
 {
-	spinlock_exit();
+	critical_exit();
 }
 
 #if defined(__i386__) || defined(__amd64__)
@@ -21,16 +21,10 @@ local_save_flags(void)
 	return (read_rflags());
 }
 
-static inline unsigned long
-_local_irq_save(void)
-{
-	return (intr_disable());
-}
-
 static inline void
 local_irq_restore(unsigned long flags __unused)
 {
-	spinlock_exit();
+	critical_exit();
 }
 #else
 #error "local_irq functions undefined"
@@ -38,7 +32,7 @@ local_irq_restore(unsigned long flags __unused)
 
 #define local_irq_save(flags) do {		\
 		flags = local_save_flags();	\
-		spinlock_enter();		\
+		critical_enter();		\
 	} while (0)
 
 #endif	/* _LINUX_IRQFLAGS_H_ */
