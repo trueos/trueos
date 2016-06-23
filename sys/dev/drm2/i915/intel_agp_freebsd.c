906a6ab49bb45045bde52ae0dccf8f9ff10b483e
Matt Macy <mmacy@nextbsd.org>
2016-06-22 19:46:35 -0700

    add rudimentary AGP bits

--- sys/dev/drm2/i915/intel_agp_freebsd.c
+++ sys/dev/drm2/i915/intel_agp_freebsd.c
@@ -17,28 +17,144 @@ __FBSDID("$FreeBSD$");
 #include <linux/vga_switcheroo.h>
 #include <drm/drm_crtc_helper.h>
 
+void *intel_gtt_get_registers(void);
+void _intel_gtt_get(size_t *gtt_total, size_t *stolen_size, unsigned long *mappable_end);
+
+#define AGP_I810_PGTBL_CTL	0x2020
+#define	AGP_I810_PGTBL_ENABLED	0x00000001
+#define INTEL_GTT_GEN	intel_private.gen
+#define GFX_FLSH_CNTL_BSD	0x2170 /* 915+ only */
+#define HAS_PGTBL_EN		1
+#define I915_GMADR_BAR	2
 
 #define WARN_UN() log(LOG_WARNING, "%s unimplemented", __FUNCTION__)
+static struct _intel_private {
+	struct pci_dev *bridge_dev;
+	u8 __iomem *registers;
+	u32 PGTBL_save;
+	int gen;
+	phys_addr_t gma_bus_addr;
+} intel_private;
+
+#ifdef __notyet__
+static struct _intel_private {
+	const struct intel_gtt_driver *driver;
+	struct pci_dev *pcidev;	/* device one */
+	struct pci_dev *bridge_dev;
+	u8 __iomem *registers;
+	phys_addr_t gtt_bus_addr;
+	u32 PGETBL_save;
+	u32 __iomem *gtt;		/* I915G */
+	bool clear_fake_agp; /* on first access via agp, fill with scratch */
+	int num_dcache_entries;
+	void __iomem *i9xx_flush_page;
+	char *i81x_gtt_table;
+	struct resource ifp_resource;
+	int resource_valid;
+	struct page *scratch_page;
+	phys_addr_t scratch_page_dma;
+	int refcount;
+	/* Whether i915 needs to use the dmar apis or not. */
+	unsigned int needs_dmar : 1;
+	phys_addr_t gma_bus_addr;
+	/*  Size of memory reserved for graphics by the BIOS */
+	unsigned int stolen_size;
+	/* Total number of gtt entries. */
+	unsigned int gtt_total_entries;
+	/* Part of the gtt that is mappable by the cpu, for those chips where
+	 * this is not the full gtt. */
+	unsigned int gtt_mappable_entries;
+} intel_private;
+#endif
+
+void
+intel_gtt_get(size_t *gtt_total, size_t *stolen_size,
+		  bus_addr_t *mappable_base, unsigned long *mappable_end)
+
+{
+	_intel_gtt_get(gtt_total, stolen_size, mappable_end);	
+	*mappable_base = intel_private.gma_bus_addr;
+	DRM_DEBUG("gtt_total %lx, stolen_size %lx, mappable_end %lx gma_bus_addr %lx\n",
+		  *gtt_total, *stolen_size, *mappable_end, *mappable_base);
+}
 
 bool
 intel_enable_gtt(void)
 {
-	WARN_UN();
-	return (false);
+	u8 __iomem *reg;
+
+
+	DRM_DEBUG("entering %s\n", __func__);
+#ifdef __notyet__
+	/* too old */
+	if (INTEL_GTT_GEN == 2) {
+		u16 gmch_ctrl;
+
+		pci_read_config_word(intel_private.bridge_dev,
+				     I830_GMCH_CTRL, &gmch_ctrl);
+		gmch_ctrl |= I830_GMCH_ENABLED;
+		pci_write_config_word(intel_private.bridge_dev,
+				      I830_GMCH_CTRL, gmch_ctrl);
+
+		pci_read_config_word(intel_private.bridge_dev,
+				     I830_GMCH_CTRL, &gmch_ctrl);
+		if ((gmch_ctrl & I830_GMCH_ENABLED) == 0) {
+			dev_err(&intel_private.pcidev->dev,
+				"failed to enable the GTT: GMCH_CTRL=%x\n",
+				gmch_ctrl);
+			return false;
+		}
+	}
+#endif	
+
+	/* On the resume path we may be adjusting the PGTBL value, so
+	 * be paranoid and flush all chipset write buffers...
+	 */
+	if (INTEL_GTT_GEN >= 3)
+		writel(0, intel_private.registers+ GFX_FLSH_CNTL_BSD);
+
+	reg = intel_private.registers+AGP_I810_PGTBL_CTL;
+	writel(intel_private.PGTBL_save, reg);
+#ifdef __notyet	
+	if (HAS_PGTBL_EN && (readl(reg) & AGP_I810_PGTBL_ENABLED) == 0) {
+		dev_err(&intel_private.pcidev->dev,
+			"failed to enable the GTT: PGTBL=%x [expected %x]\n",
+			readl(reg), intel_private.PGTBL_save);
+		return false;
+	}
+#endif	
+
+	if (INTEL_GTT_GEN >= 3)
+		writel(0, intel_private.registers+ GFX_FLSH_CNTL_BSD);
+	DRM_DEBUG("exiting %s\n", __func__);
+	return (1);
 }
 
 int
 intel_gmch_probe(struct pci_dev *bridge_pdev, struct pci_dev *gpu_pdev,
 		 struct agp_bridge_data *bridge)
 {
-	WARN_UN();
-	return (0);
+	DRM_DEBUG("entering %s\n", __func__);
+	intel_private.registers = intel_gtt_get_registers(); 
+	intel_private.gma_bus_addr = pci_bus_address(gpu_pdev, I915_GMADR_BAR);
+	DRM_DEBUG("bus_addr %lx\n", intel_private.gma_bus_addr);
+	INTEL_GTT_GEN = 4;
+	/* save the PGTBL reg for resume */
+	intel_private.PGTBL_save =
+		readl(intel_private.registers+AGP_I810_PGTBL_CTL)
+			& ~AGP_I810_PGTBL_ENABLED;
+	/* we only ever restore the register when enabling the PGTBL... */
+	if (HAS_PGTBL_EN)
+		intel_private.PGTBL_save |= AGP_I810_PGTBL_ENABLED;
+
+	DRM_DEBUG("exiting %s\n", __func__);
+	return (1);
 }
 
 void
 intel_gmch_remove(void)
 {
-	WARN_UN();
+
 }
 
 void
