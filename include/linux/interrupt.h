98b8cb99c569c31b31b5e44d21eee5f452b849e3
Matt Macy <mmacy@nextbsd.org>
2016-08-10 19:33:44 -0700

    add tasklet support

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -164,4 +164,81 @@ free_irq(unsigned int irq, void *device)
 }
 
 #define resource linux_resource
+
+/* Tasklets --- multithreaded analogue of BHs.
+
+   Main feature differing them of generic softirqs: tasklet
+   is running only on one CPU simultaneously.
+
+   Main feature differing them of BHs: different tasklets
+   may be run simultaneously on different CPUs.
+
+   Properties:
+   * If tasklet_schedule() is called, then tasklet is guaranteed
+     to be executed on some cpu at least once after this.
+   * If the tasklet is already scheduled, but its execution is still not
+     started, it will be executed only once.
+   * If this tasklet is already running on another CPU (or schedule is called
+     from tasklet itself), it is rescheduled for later.
+   * Tasklet is strictly serialized wrt itself, but not
+     wrt another tasklets. If client needs some intertask synchronization,
+     he makes it with spinlocks.
+ */
+
+struct tasklet_struct
+{
+	struct tasklet_struct *next;
+	unsigned long state;
+	atomic_t count;
+	void (*func)(unsigned long);
+	unsigned long data;
+};
+
+#define DECLARE_TASKLET(name, func, data) \
+struct tasklet_struct name = { NULL, 0, ATOMIC_INIT(0), func, data }
+
+#define DECLARE_TASKLET_DISABLED(name, func, data) \
+struct tasklet_struct name = { NULL, 0, ATOMIC_INIT(1), func, data }
+
+
+enum
+{
+	TASKLET_STATE_SCHED,	/* Tasklet is scheduled for execution */
+	TASKLET_STATE_RUN	/* Tasklet is running (SMP only) */
+};
+
+#ifdef CONFIG_SMP
+static inline int tasklet_trylock(struct tasklet_struct *t)
+{
+	return !test_and_set_bit(TASKLET_STATE_RUN, &(t)->state);
+}
+
+static inline void tasklet_unlock(struct tasklet_struct *t)
+{
+	smp_mb__before_atomic();
+	clear_bit(TASKLET_STATE_RUN, &(t)->state);
+}
+
+static inline void tasklet_unlock_wait(struct tasklet_struct *t)
+{
+	while (test_bit(TASKLET_STATE_RUN, &(t)->state)) { barrier(); }
+}
+#else
+#define tasklet_trylock(t) 1
+#define tasklet_unlock_wait(t) do { } while (0)
+#define tasklet_unlock(t) do { } while (0)
+#endif
+
+extern void __tasklet_schedule(struct tasklet_struct *t);
+
+static inline void tasklet_schedule(struct tasklet_struct *t)
+{
+	if (!test_and_set_bit(TASKLET_STATE_SCHED, &t->state))
+		__tasklet_schedule(t);
+}
+
+extern void tasklet_kill(struct tasklet_struct *t);
+extern void tasklet_init(struct tasklet_struct *t,
+			 void (*func)(unsigned long), unsigned long data);
+
 #endif	/* _LINUX_INTERRUPT_H_ */
---
+++ sys/compat/linuxkpi/common/src/linux_tasklet.c
@@ -0,0 +1,98 @@
+#include <sys/types.h>
+#include <sys/malloc.h>
+#include <sys/gtaskqueue.h>
+
+#include <linux/interrupt.h>
+struct aligned_ptr {
+	void *ptr;
+} __aligned(CACHE_LINE_SIZE);
+
+static struct grouptask_aligned *tasklet_gtask_array;
+static struct aligned_ptr *tasklet_head_array;
+
+static void
+tasklet_handler(void *arg)
+{
+	intptr_t cpuid = (intptr_t)arg;
+	struct tasklet_struct *t, *tprev;
+
+	MPASS(curcpu == cpuid);
+	disable_intr();
+	t = tasklet_head_array[cpuid].ptr;
+	tasklet_head_array[cpuid].ptr = NULL;
+	enable_intr();
+	while (t != NULL) {
+		if (tasklet_trylock(t)) {
+			t->func(t->data);
+			tprev = t;
+			t = t->next;
+			tasklet_unlock(tprev);
+		} else
+			t = t->next;
+	}
+}
+
+static void
+tasklet_subsystem_init(void *arg __unused)
+{
+	int i;
+	struct grouptask *gtask;
+	char buf[32];
+
+	tasklet_gtask_array = malloc(sizeof(struct grouptask_aligned)*mp_ncpus, M_KMALLOC, M_WAITOK|M_ZERO);
+	tasklet_head_array =  malloc(CACHE_LINE_SIZE*mp_ncpus, M_KMALLOC, M_WAITOK|M_ZERO);
+
+	for (i = 0; i < mp_ncpus; i++) {
+		if (CPU_ABSENT(i))
+			continue;
+		gtask = (struct grouptask *)&tasklet_gtask_array[i];
+		GROUPTASK_INIT(gtask, 0, tasklet_handler, (void *)(uintptr_t)i);
+		snprintf(buf, 31, "softirq%d", i);
+		taskqgroup_attach_cpu(qgroup_softirq, gtask, "tasklet", i, -1, buf);
+	}
+}
+SYSINIT(linux_tasklet, SI_SUB_KTHREAD_PAGE, SI_ORDER_SECOND, tasklet_subsystem_init, NULL);
+
+void
+tasklet_init(struct tasklet_struct *t, void (*func)(unsigned long), unsigned long data)
+{
+	t->next = NULL;
+	t->state = 0;
+	atomic_set(&t->count, 0);
+	t->func = func;
+	t->data = data;	
+}
+
+void
+__tasklet_schedule(struct tasklet_struct *t)
+{
+	struct tasklet_struct *ttmp;
+	struct grouptask *gtask;
+	int cpuid, inintr;
+
+	inintr = curthread->td_intr_nesting_level;
+
+	if (!inintr)
+		disable_intr();
+	cpuid = curcpu;
+	gtask = (struct grouptask *)&tasklet_gtask_array[cpuid];
+	ttmp = tasklet_head_array[cpuid].ptr;
+	t->next = ttmp;
+	tasklet_head_array[cpuid].ptr = t;
+	if (!inintr)
+		enable_intr();
+	GROUPTASK_ENQUEUE(gtask);
+}
+
+void
+tasklet_kill(struct tasklet_struct *t)
+{
+	while (test_and_set_bit(TASKLET_STATE_SCHED, &t->state)) {
+		do {
+			kern_yield(0);
+		} while (test_bit(TASKLET_STATE_SCHED, &t->state));
+	}
+	tasklet_unlock_wait(t);
+	clear_bit(TASKLET_STATE_SCHED, &t->state);	
+}
+
