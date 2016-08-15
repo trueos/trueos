46ed55c48fbfdfc5c26dc9e1c5570db84af4eceb
Chris Wilson <chris@chris-wilson.co.uk>
2016-08-15 10:48:54 +0100

    drm/i915: Use VMA as the primary object for context state
    When working with contexts, we most frequently want the GGTT VMA for the
    context state, first and foremost. Since the object is available via the
    VMA, we need only then store the VMA.
    
    v2: Formatting tweaks to debugfs output, restored some comments removed
    in the next patch
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1471254551-25805-15-git-send-email-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gpu_error.c
+++ drivers/gpu/drm/i915/i915_gpu_error.c
@@ -1107,9 +1107,10 @@ static void i915_gem_record_rings(struct drm_i915_private *dev_priv,
 					i915_error_ggtt_object_create(dev_priv,
 								      engine->scratch.obj);
 
-			ee->ctx =
-				i915_error_ggtt_object_create(dev_priv,
-							      request->ctx->engine[i].state);
+			if (request->ctx->engine[i].state) {
+				ee->ctx = i915_error_ggtt_object_create(dev_priv,
+									request->ctx->engine[i].state->obj);
+			}
 
 			if (request->pid) {
 #ifdef __linux__
