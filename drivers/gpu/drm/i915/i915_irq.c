4dd18d1f4281e6ea8b0df9b4e7015fb06c18b8a4
Chris Wilson <chris@chris-wilson.co.uk>
2016-08-27 08:54:01 +0100

    drm/i915: Make for_each_engine_masked() more compact and quicker
    Rather than walk the full array of engines checking whether each is in
    the mask in turn, we can use the mask to jump to the right engines. This
    should quicker for a sparse array of engines or mask, whilst generating
    smaller code:
    
       text	   data	    bss	    dec	    hex	filename
    1251010	   4579	    800	1256389	 132bc5	drivers/gpu/drm/i915/i915.ko
    1250530	   4579	    800	1255909	 1329e5	drivers/gpu/drm/i915/i915.ko
    
    The downside is that we have to pass in a temporary, alas no C99
    iterators yet.
    
    [P.S. Joonas doesn't like having to pass extra temporaries into the
    macro, and even less that I called them tmp. As yet, we haven't found a
    macro that avoids passing in a temporary that is smaller. We probably
    will get C99 iterators first!]
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20160827075401.16470-2-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -3173,6 +3173,7 @@ static void i915_hangcheck_elapsed(struct work_struct *work)
 
 	if (hung) {
 		char msg[80];
+		unsigned int tmp;
 		int len;
 
 		/* If some rings hung but others were still busy, only
@@ -3182,7 +3183,7 @@ static void i915_hangcheck_elapsed(struct work_struct *work)
 			hung &= ~stuck;
 		len = scnprintf(msg, sizeof(msg),
 				"%s on ", stuck == hung ? "No progress" : "Hang");
-		for_each_engine_masked(engine, dev_priv, hung)
+		for_each_engine_masked(engine, dev_priv, hung, tmp)
 			len += scnprintf(msg + len, sizeof(msg) - len,
 					 "%s, ", engine->name);
 		msg[len-2] = '\0';
