9018bb8925280c5b5503c45939dc6aad144592e4
Matt Macy <mmacy@nextbsd.org>
2016-08-29 23:57:58 -0700

    remove fence-array references only needed by 4.8+

--- sys/compat/linuxkpi/common/src/linux_fence.c
+++ sys/compat/linuxkpi/common/src/linux_fence.c
@@ -1,5 +1,4 @@
 #include <linux/fence.h>
-#include <linux/fence-array.h>
 
 #include <sys/param.h>
 #include <sys/proc.h>
@@ -13,27 +12,4 @@ static unsigned fence_counter = 0;
 unsigned
 fence_context_alloc(unsigned num){
 	return atomic_fetchadd_int(&fence_counter, num);
-}
-
-struct fence_array *fence_array_create(int num_fences, struct fence **fences,
-				       u64 context, unsigned seqno,
-				       bool signal_on_any)
-{
-	struct fence_array *array;
-	size_t size = sizeof(*array);
-
-	size += num_fences * sizeof(struct fence_array_cb);
-	array = kzalloc(size, GFP_KERNEL);
-	if (!array)
-		return NULL;
-
-	spin_lock_init(&array->lock);
-	fence_init(&array->base, &fence_array_ops, &array->lock,
-		   context, seqno);
-
-	array->num_fences = num_fences;
-	atomic_set(&array->num_pending, signal_on_any ? 1 : num_fences);
-	array->fences = fences;
-
-	return (array);
 }
