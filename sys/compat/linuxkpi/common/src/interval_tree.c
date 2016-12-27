cb961ea341b4d48bbdfd3d5dde4b99c38b72130c
Matthew Macy <mmacy@kvasirtech.com>
2016-12-26 20:25:55 -0800

    add extra layer of interval tree calls to facilitate debugging

--- sys/compat/linuxkpi/common/src/interval_tree.c
+++ sys/compat/linuxkpi/common/src/interval_tree.c
@@ -177,7 +177,49 @@ interval_tree_iter_next(struct interval_tree_node *node,
 #define START(node) ((node)->start)
 #define LAST(node)  ((node)->last)
 
+static void
+interval_tree__insert(struct interval_tree_node *node, struct rb_root *root);
+
+static void
+interval_tree__remove(struct interval_tree_node *node, struct rb_root *root);
+
+static struct interval_tree_node *
+interval_tree__iter_first(struct rb_root *root,
+			 unsigned long start, unsigned long last);
+
+static struct interval_tree_node *
+interval_tree__iter_next(struct interval_tree_node *node,
+			unsigned long start, unsigned long last);
+
+
+
 INTERVAL_TREE_DEFINE(struct interval_tree_node, rb,
                      unsigned long, __subtree_last,
-                     START, LAST,, interval_tree)
+                     START, LAST,, interval_tree_)
+
+void
+interval_tree_insert(struct interval_tree_node *node, struct rb_root *root)
+{
+	interval_tree__insert(node, root);
+}
+
+void
+interval_tree_remove(struct interval_tree_node *node, struct rb_root *root)
+{
+	interval_tree__remove(node, root);
+}
+
+struct interval_tree_node *
+interval_tree_iter_first(struct rb_root *root,
+			 unsigned long start, unsigned long last)
+{
+	return interval_tree__iter_first(root, start, last);
+}
+
+struct interval_tree_node *
+interval_tree_iter_next(struct interval_tree_node *node,
+			unsigned long start, unsigned long last)
+{
+	return interval_tree__iter_next(node, start, last);
+}
 #endif
