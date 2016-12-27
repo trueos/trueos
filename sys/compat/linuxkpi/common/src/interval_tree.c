3dbc9e6575eb74a158884a6d4edbab3cb4abd784
Matthew Macy <mmacy@kvasirtech.com>
2016-12-26 17:48:32 -0800

    switch to generic INTERVAL_TREE usage

--- sys/compat/linuxkpi/common/src/interval_tree.c
+++ sys/compat/linuxkpi/common/src/interval_tree.c
@@ -1,3 +1,4 @@
+#ifdef __notyet__
 #define RB_AUGMENT(entry) interval_tree_augment_entry(entry)
 
 
@@ -167,3 +168,16 @@ interval_tree_iter_next(struct interval_tree_node *node,
 
 	return (NULL);
 }
+#else
+#include <linux/interval_tree.h>
+#include <linux/interval_tree_generic.h>
+#include <linux/compiler.h>
+#include <linux/export.h>
+
+#define START(node) ((node)->start)
+#define LAST(node)  ((node)->last)
+
+INTERVAL_TREE_DEFINE(struct interval_tree_node, rb,
+                     unsigned long, __subtree_last,
+                     START, LAST,, interval_tree)
+#endif
