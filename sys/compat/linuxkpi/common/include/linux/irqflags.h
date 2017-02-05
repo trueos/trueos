1990f4bd9f892384b3b3408d4029da1c1620a4f4
Hans Petter Selasky <hps@selasky.org>
2017-02-05 14:38:25 +0100

    Stub all IRQ disable functions.
    To synchronize functions mutexes should be used.
    To guarantee execution time use raised thread priority.
    
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- sys/compat/linuxkpi/common/include/linux/irqflags.h
+++ sys/compat/linuxkpi/common/include/linux/irqflags.h
@@ -1,38 +1,10 @@
 #ifndef _LINUX_IRQFLAGS_H_
-#define _LINUX_IRQFLAGS_H_
+#define	_LINUX_IRQFLAGS_H_
 
+#define	local_irq_disable(void)		do { } while (0)
+#define	local_irq_enable(void)		do { } while (0)
+#define	local_save_flags(void)		0
+#define	local_irq_restore(flags)	do { } while (0)
+#define	local_irq_save(flags)		do { (flags) = 0; } while (0)
 
-static inline void
-local_irq_disable(void)
-{
-	spinlock_enter();
-}
-
-static inline void
-local_irq_enable(void)
-{
-	spinlock_exit();
-}
-
-#if defined(__i386__) || defined(__amd64__)
-static inline unsigned long
-local_save_flags(void)
-{
-	return (read_rflags());
-}
-
-static inline void
-local_irq_restore(unsigned long flags __unused)
-{
-	spinlock_exit();
-}
-#else
-#error "local_irq functions undefined"
-#endif
-
-#define local_irq_save(flags) do {		\
-		flags = local_save_flags();	\
-		spinlock_enter();		\
-	} while (0)
-
-#endif	/* _LINUX_IRQFLAGS_H_ */
+#endif					/* _LINUX_IRQFLAGS_H_ */
