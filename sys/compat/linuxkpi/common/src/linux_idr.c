6b791a1d7bca40040bd7b2d964192810b55c91c8
Hans Petter Selasky <hps@selasky.org>
2017-02-06 19:41:44 +0100

    Reimplement IDR preload to use a set of per-CPU spinlocks instead of a critical section.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- sys/compat/linuxkpi/common/src/linux_idr.c
+++ sys/compat/linuxkpi/common/src/linux_idr.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2010 Isilon Systems, Inc.
  * Copyright (c) 2010 iX Systems, Inc.
  * Copyright (c) 2010 Panasas, Inc.
- * Copyright (c) 2013-2016 Mellanox Technologies, Ltd.
+ * Copyright (c) 2013-2017 Mellanox Technologies, Ltd.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -46,15 +46,18 @@ __FBSDID("$FreeBSD$");
 #include <linux/slab.h>
 #include <linux/idr.h>
 #include <linux/err.h>
+#include <linux/compat.h>
 
+#define	MAX_IDR_LEVEL	((MAX_IDR_SHIFT + IDR_BITS - 1) / IDR_BITS)
+#define	MAX_IDR_FREE	(MAX_IDR_LEVEL * 2)
 
+struct linux_idr_cache {
+	spinlock_t lock;
+	struct idr_layer *head;
+	unsigned count;
+};
 
-DPCPU_DEFINE(struct idr_layer *, idr_preload_head);
-DPCPU_DEFINE(int, idr_preload_cnt);
-
-#define MAX_IDR_LEVEL ((MAX_IDR_SHIFT + IDR_BITS - 1) / IDR_BITS)
-#define MAX_IDR_FREE (MAX_IDR_LEVEL * 2)
-
+static DPCPU_DEFINE(struct linux_idr_cache, linux_idr_cache);
 
 /*
  * IDR Implementation.
@@ -65,29 +68,94 @@ DPCPU_DEFINE(int, idr_preload_cnt);
  */
 static MALLOC_DEFINE(M_IDR, "idr", "Linux IDR compat");
 
+static struct idr_layer *
+idr_preload_dequeue_locked(struct linux_idr_cache *lic)
+{
+	struct idr_layer *retval;
+
+	/* check if wrong thread is trying to dequeue */
+	if (mtx_owned(&lic->lock.m) == 0)
+		return (NULL);
+
+	retval = lic->head;
+	if (likely(retval != NULL)) {
+		lic->head = retval->ary[0];
+		lic->count--;
+		retval->ary[0] = NULL;
+	}
+	return (retval);
+}
+
+static void
+idr_preload_init(void *arg)
+{
+	int cpu;
+
+	CPU_FOREACH(cpu) {
+		struct linux_idr_cache *lic =
+		    DPCPU_ID_PTR(cpu, linux_idr_cache);
+
+		spin_lock_init(&lic->lock);
+	}
+}
+SYSINIT(idr_preload_init, SI_SUB_LOCK, SI_ORDER_FIRST, idr_preload_init, NULL);
+
+static void
+idr_preload_uninit(void *arg)
+{
+	int cpu;
+
+	CPU_FOREACH(cpu) {
+		struct idr_layer *cacheval;
+		struct linux_idr_cache *lic =
+		    DPCPU_ID_PTR(cpu, linux_idr_cache);
+
+		while (1) {
+			spin_lock(&lic->lock);
+			cacheval = idr_preload_dequeue_locked(lic);
+			spin_unlock(&lic->lock);
+
+			if (cacheval == NULL)
+				break;
+			free(cacheval, M_IDR);
+		}
+		spin_lock_destroy(&lic->lock);
+	}
+}
+SYSUNINIT(idr_preload_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, idr_preload_uninit, NULL);
+
 void
 idr_preload(gfp_t gfp_mask)
 {
+	struct linux_idr_cache *lic;
 	struct idr_layer *cacheval;
 
-	critical_enter();
-	while (DPCPU_GET(idr_preload_cnt) < MAX_IDR_FREE) {
-		critical_exit();
-		cacheval = malloc(sizeof(*cacheval), M_IDR, gfp_mask|M_ZERO);
-		critical_enter();
+	sched_pin();
+
+	lic = &DPCPU_GET(linux_idr_cache);
+
+	/* fill up cache */
+	spin_lock(&lic->lock);
+	while (lic->count < MAX_IDR_FREE) {
+		spin_unlock(&lic->lock);
+		cacheval = malloc(sizeof(*cacheval), M_IDR, M_ZERO | gfp_mask);
+		spin_lock(&lic->lock);
 		if (cacheval == NULL)
 			break;
-		cacheval->ary[0] = DPCPU_GET(idr_preload_head);
-		DPCPU_SET(idr_preload_head, cacheval);
-		DPCPU_SET(idr_preload_cnt, DPCPU_GET(idr_preload_cnt) + 1);
+		cacheval->ary[0] = lic->head;
+		lic->head = cacheval;
+		lic->count++;
 	}
-	MPASS(curthread->td_critnest == 1);
 }
 
 void
 idr_preload_end(void)
 {
-	critical_exit();
+	struct linux_idr_cache *lic;
+
+	lic = &DPCPU_GET(linux_idr_cache);
+	spin_unlock(&lic->lock);
+	sched_unpin();
 }
 
 static inline int
@@ -321,21 +389,16 @@ idr_get(struct idr *idp)
 {
 	struct idr_layer *il;
 
-	if ((il = __free_list_get(idp))) {
+	if ((il = __free_list_get(idp)) != NULL) {
 		MPASS(ffsl(il->bitmap) != 0);
-		return (il);
-	}
-	if ((il = malloc(sizeof(*il), M_IDR, M_ZERO | M_NOWAIT)) != NULL)
-		goto done;
-	if (!in_interrupt()) {
-		if ((il = DPCPU_GET(idr_preload_head)) != NULL) {
-			DPCPU_SET(idr_preload_head, il->ary[0]);
-			DPCPU_SET(idr_preload_cnt, DPCPU_GET(idr_preload_cnt) - 1);
-		}
-	}
-done:
-	if (il != NULL)
+	} else if ((il = malloc(sizeof(*il), M_IDR, M_ZERO | M_NOWAIT)) != NULL) {
 		bitmap_fill(&il->bitmap, IDR_SIZE);
+	} else if (!in_interrupt() &&
+	    (il = idr_preload_dequeue_locked(&DPCPU_GET(linux_idr_cache))) != NULL) {
+		bitmap_fill(&il->bitmap, IDR_SIZE);
+	} else {
+		return (NULL);
+	}
 	return (il);
 }
 
