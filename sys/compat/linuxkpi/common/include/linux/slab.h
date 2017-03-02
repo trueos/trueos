0d4dcdc94d5b1ef042a3eaa182f08bf5be1f551b
Hans Petter Selasky <hps@selasky.org>
2017-03-02 11:01:20 +0100

    Implement support for SLAB_DESTROY_BY_RCU to prevent use after free with i915kms. Add some missing gfp_t flag checks for valid use of M_WAITOK and M_NOWAIT while at it. This is a preparation step for upstreaming.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- sys/compat/linuxkpi/common/include/linux/slab.h
+++ sys/compat/linuxkpi/common/include/linux/slab.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2010 Isilon Systems, Inc.
  * Copyright (c) 2010 iX Systems, Inc.
  * Copyright (c) 2010 Panasas, Inc.
- * Copyright (c) 2013, 2014 Mellanox Technologies, Ltd.
+ * Copyright (c) 2013-2017 Mellanox Technologies, Ltd.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -67,51 +67,18 @@ MALLOC_DECLARE(M_KMALLOC);
 #define	kmem_cache_free(...) 	linux_kmem_cache_free(__VA_ARGS__)
 #define	kmem_cache_destroy(...) linux_kmem_cache_destroy(__VA_ARGS__)
 
+typedef void linux_kmem_ctor_t (void *);
+
 struct linux_kmem_cache {
-	uma_zone_t	cache_zone;
-	void		(*cache_ctor)(void *);
+	uma_zone_t cache_zone;
+	linux_kmem_ctor_t *cache_ctor;
+	unsigned cache_flags;
+	unsigned cache_size;
 };
 
-#define	SLAB_HWCACHE_ALIGN	0x0001
-/*                                                                                                        
- * SLAB_DESTROY_BY_RCU - **WARNING** READ THIS!                                                           
- *                                                                                                        
- * This delays freeing the SLAB page by a grace period, it does _NOT_                                     
- * delay object freeing. This means that if you do kmem_cache_free()                                      
- * that memory location is free to be reused at any time. Thus it may                                     
- * be possible to see another object there in the same RCU grace period.                                  
- *                                                                                                        
- * This feature only ensures the memory location backing the object                                       
- * stays valid, the trick to using this is relying on an independent                                      
- * object validation pass. Something like:                                                                
- *                                                                                                        
- *  rcu_read_lock()                                                                                       
- * again:                                                                                                 
- *  obj = lockless_lookup(key);                                                                           
- *  if (obj) {                                                                                            
- *    if (!try_get_ref(obj)) // might fail for free objects                                               
- *      goto again;                                                                                       
- *                                                                                                        
- *    if (obj->key != key) { // not the object we expected                                                
- *      put_ref(obj);                                                                                     
- *      goto again;                                                                                       
- *    }                                                                                                   
- *  }                                                                                                     
- *  rcu_read_unlock();                                                                                    
- *                                                                                                        
- * This is useful if we need to approach a kernel structure obliquely,                                    
- * from its address obtained without the usual locking. We can lock                                       
- * the structure to stabilize it and check it's still at the given address,                               
- * only if we can be sure that the memory has not been meanwhile reused                                   
- * for some other kind of object (which our subsystem's lock might corrupt).                              
- *                                                                                                        
- * rcu_read_lock before reading the address, then rcu_read_unlock after                                   
- * taking the spinlock within the structure expected at that address.                                     
- */
-#define SLAB_DESTROY_BY_RCU     0x00080000UL    /* Defer freeing slabs to RCU */
-/* The following flags affect the page allocator grouping pages by mobility */
-#define SLAB_RECLAIM_ACCOUNT    0x00020000UL            /* Objects are reclaimable */
-
+#define	SLAB_HWCACHE_ALIGN	(1 << 0)
+#define	SLAB_DESTROY_BY_RCU     (1 << 1)
+#define	SLAB_RECLAIM_ACCOUNT	(1 << 2)
 
 static inline void *
 kmalloc(size_t size, gfp_t flags)
@@ -154,58 +121,47 @@ kfree(const void *ptr)
 	free(__DECONST(void *, ptr), M_KMALLOC);
 }
 
-static inline int
-linux_kmem_ctor(void *mem, int size, void *arg, int flags)
+extern struct linux_kmem_cache *
+linux_kmem_cache_create(const char *name, size_t size,
+    size_t align, unsigned flags, linux_kmem_ctor_t *ctor);
+
+static inline void *
+linux_kmem_cache_alloc(struct linux_kmem_cache *c, gfp_t flags)
 {
-	void (*ctor)(void *);
+	const gfp_t m = M_NOWAIT | M_WAITOK;
 
-	ctor = arg;
-	ctor(mem);
+	if ((flags & m) == 0)
+		flags |= M_NOWAIT;
+	else if ((flags & m) == m)
+		flags &= ~M_WAITOK;
 
-	return (0);
-}
-
-static inline struct kmem_cache *
-linux_kmem_cache_create(char *name, size_t size, size_t align, u_long flags,
-    void (*ctor)(void *))
-{
-	struct kmem_cache *c;
-
-	c = malloc(sizeof(*c), M_KMALLOC, M_WAITOK);
-	if (align)
-		align--;
-	if (flags & SLAB_HWCACHE_ALIGN)
-		align = UMA_ALIGN_CACHE;
-	c->cache_zone = uma_zcreate(name, size, ctor ? linux_kmem_ctor : NULL,
-	    NULL, NULL, NULL, align, 0);
-	c->cache_ctor = ctor;
-
-	return (c);
+	return (uma_zalloc_arg(c->cache_zone, c, flags));
 }
 
 static inline void *
-linux_kmem_cache_alloc(struct kmem_cache *c, int flags)
+kmem_cache_zalloc(struct linux_kmem_cache *c, gfp_t flags)
 {
-	return uma_zalloc_arg(c->cache_zone, c->cache_ctor, (flags ? flags : M_NOWAIT));
+	const gfp_t m = M_NOWAIT | M_WAITOK;
+
+	if ((flags & m) == 0)
+		flags |= M_NOWAIT;
+	else if ((flags & m) == m)
+		flags &= ~M_WAITOK;
+
+	return (uma_zalloc_arg(c->cache_zone, c, flags | M_ZERO));
 }
 
-static inline void *
-kmem_cache_zalloc(struct kmem_cache *c, int flags)
-{
-	return uma_zalloc_arg(c->cache_zone, c->cache_ctor, (flags ? flags : M_NOWAIT) |M_ZERO);
-}
-
-static inline void
-linux_kmem_cache_free(struct kmem_cache *c, void *m)
-{
-	uma_zfree(c->cache_zone, m);
-}
+extern void linux_kmem_cache_free_rcu(struct linux_kmem_cache *, void *);
 
 static inline void
-linux_kmem_cache_destroy(struct kmem_cache *c)
+linux_kmem_cache_free(struct linux_kmem_cache *c, void *m)
 {
-	uma_zdestroy(c->cache_zone);
-	free(c, M_KMALLOC);
+	if (unlikely(c->cache_flags & SLAB_DESTROY_BY_RCU))
+		linux_kmem_cache_free_rcu(c, m);
+	else
+		uma_zfree(c->cache_zone, m);
 }
 
-#endif	/* _LINUX_SLAB_H_ */
+extern void linux_kmem_cache_destroy(struct linux_kmem_cache *);
+
+#endif					/* _LINUX_SLAB_H_ */
