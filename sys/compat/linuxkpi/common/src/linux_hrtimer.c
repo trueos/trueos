c307c353c4a496b9fcbad5f5558a2f65ab5d681d
Mark Johnston <markjdb@gmail.com>
2017-03-22 23:14:10 -0700

    Re-implement a subset of the hrtimer API from scratch.
    This lets us move some code into linuxkpi_gplv2 as well as remove some
    local modifications to the callout code.

--- sys/compat/linuxkpi/common/src/linux_hrtimer.c
+++ sys/compat/linuxkpi/common/src/linux_hrtimer.c
@@ -1,81 +1,109 @@
-#include <sys/types.h>
-#include <sys/systm.h>
+/*-
+ * Copyright (c) 2017 Mark Johnston <markj@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <linux/hrtimer.h>
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/time.h>
+
 #include <machine/cpu.h>
 
-CTASSERT(sizeof(struct callout) <= BSD_CALLOUT_SIZE);
+#include <linux/hrtimer.h>
+
+/* hrtimer flags */
+#define	HRTIMER_ACTIVE		0x01
 
 static void
-__hrtimer_handler(void *arg)
+hrtimer_call_handler(void *arg)
 {
-	struct hrtimer *h = arg;
-	enum hrtimer_restart rc;
-	int flags;
-	struct callout *c = __DECONST(struct callout *, &h->bsd_callout_);
+	struct hrtimer *hrtimer;
+	enum hrtimer_restart ret;
 
-	flags = C_PREL(0);
-	rc = h->function(h);
-	if (rc == HRTIMER_RESTART)
-		callout_reset_sbt(c, c->c_time, c->c_precision, __hrtimer_handler, h, flags);
-}
-
-int
-hrtimer_try_to_cancel(struct hrtimer *timer)
-{
-	struct callout *c;
-
-	if (!hrtimer_active(timer))
-		return (0);
-
-	c = (struct callout *)&timer->bsd_callout_;
-	return -(callout_stop(c) == 0);
-}
-
-void
-hrtimer_init(struct hrtimer *timer, clockid_t which_clock,  enum hrtimer_mode mode)
-{
-	struct callout *c = __DECONST(struct callout *, &timer->bsd_callout_);
-
-	/* we only support MONOTONIC */
-	MPASS(which_clock == CLOCK_MONOTONIC);
-
-	memset(timer, 0, sizeof(struct hrtimer));
-	callout_init(c, 1 /* mpsafe */);
-}
-
-void
-hrtimer_start_range_ns(struct hrtimer *timer, ktime_t tim, u64 range_ns, const enum hrtimer_mode mode)
-{
-	struct callout *c = __DECONST(struct callout *, &timer->bsd_callout_);
-	int flags;
-
-	flags = C_PREL(0);
-	if (mode == HRTIMER_MODE_ABS ||
-	    mode == HRTIMER_MODE_ABS_PINNED)
-		flags |= C_ABSOLUTE;
-		
-	callout_reset_sbt(c, tim.tv64*SBT_1NS, range_ns*SBT_1NS,  __hrtimer_handler, timer, flags);
+	hrtimer = arg;
+	ret = hrtimer->function(hrtimer);
+	MPASS(ret == HRTIMER_NORESTART);
+	hrtimer->flags &= ~HRTIMER_ACTIVE;
 }
 
 bool
-hrtimer_active(const struct hrtimer *timer)
+linux_hrtimer_active(struct hrtimer *hrtimer)
 {
-	struct callout *c = __DECONST(struct callout *, &timer->bsd_callout_);
+	bool ret;
 
-	return (callout_running(c));
+	mtx_lock(&hrtimer->mtx);
+	ret = (hrtimer->flags & HRTIMER_ACTIVE) != 0;
+	mtx_unlock(&hrtimer->mtx);
+	return (ret);
 }
 
 int
-hrtimer_cancel(struct hrtimer *timer)
+linux_hrtimer_cancel(struct hrtimer *hrtimer)
 {
-	int rc;
 
-	for (;;) {
-		rc = hrtimer_try_to_cancel(timer);
+	if (!hrtimer_active(hrtimer))
+		return (0);
+	(void)callout_drain(&hrtimer->callout);
+	return (1);
+}
 
-		if (rc >= 0)
-			return (rc);
-		cpu_spinwait();
-	}	
+void
+linux_hrtimer_init(struct hrtimer *hrtimer)
+{
+
+	hrtimer->function = NULL;
+
+	hrtimer->flags = 0;
+	mtx_init(&hrtimer->mtx, "hrtimer", NULL, MTX_DEF | MTX_RECURSE);
+	callout_init_mtx(&hrtimer->callout, &hrtimer->mtx, 0);
+}
+
+void
+linux_hrtimer_set_expires(struct hrtimer *hrtimer __unused,
+    ktime_t time __unused)
+{
+}
+
+void
+linux_hrtimer_start(struct hrtimer *hrtimer, ktime_t time)
+{
+
+	linux_hrtimer_start_range_ns(hrtimer, time, 0);
+}
+
+void
+linux_hrtimer_start_range_ns(struct hrtimer *hrtimer, ktime_t time, long nsec)
+{
+
+	mtx_lock(&hrtimer->mtx);
+	callout_reset_sbt(&hrtimer->callout, time.tv64 * SBT_1NS,
+	    nsec * SBT_1NS, hrtimer_call_handler, hrtimer, 0);
+	hrtimer->flags |= HRTIMER_ACTIVE;
+	mtx_unlock(&hrtimer->mtx);
 }
