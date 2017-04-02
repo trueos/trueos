28466976bc829efe9dea1f28948ac92633bd0bbf
Mark Johnston <markjdb@gmail.com>
2017-04-01 17:06:40 -0700

    Move some headers to linuxkpi_gplv2; restore mipi_display.h header.

--- sys/compat/linuxkpi/gplv2/include/video/mipi_display.h
+++ sys/compat/linuxkpi/gplv2/include/video/mipi_display.h
@@ -1,3 +1,15 @@
+/*
+ * Defines for Mobile Industry Processor Interface (MIPI(R))
+ * Display Working Group standards: DSI, DCS, DBI, DPI
+ *
+ * Copyright (C) 2010 Guennadi Liakhovetski <g.liakhovetski@gmx.de>
+ * Copyright (C) 2006 Nokia Corporation
+ * Author: Imre Deak <imre.deak@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 #ifndef MIPI_DISPLAY_H
 #define MIPI_DISPLAY_H
 
