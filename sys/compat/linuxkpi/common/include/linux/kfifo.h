7eb4949eb620a58e568921b49a9f27533c347a15
Mark Johnston <markjdb@gmail.com>
2017-04-08 13:36:32 -0700

    Restore the license header for kfifo.h.

--- sys/compat/linuxkpi/common/include/linux/kfifo.h
+++ sys/compat/linuxkpi/common/include/linux/kfifo.h
@@ -1,7 +1,24 @@
 /*
- * Linux's kfifo implementation
- * Doc comments removed as no one should be programming to this interface
+ * A generic kernel FIFO implementation
+ *
+ * Copyright (C) 2013 Stefani Seibold <stefani@seibold.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
+
 #ifndef _LINUX_KFIFO_H
 #define _LINUX_KFIFO_H
 #include <linux/kernel.h>
