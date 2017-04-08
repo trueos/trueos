191cab591b7ce2b9be33637a3387354b985dea72
Mark Johnston <markjdb@gmail.com>
2017-04-08 16:13:28 -0700

    Move component.h to linuxkpi_gplv2.

--- sys/compat/linuxkpi/common/src/linux_irq.c
+++ sys/compat/linuxkpi/common/src/linux_irq.c
@@ -1,24 +1,16 @@
-
-
 #include <linux/irq.h>
 #include <linux/irqdomain.h>
-#include <linux/component.h>
 
 DECLARE_BITMAP(allocated_irqs, IRQ_BITMAP_BITS);
 int nr_irqs;
 DEFINE_MUTEX(irq_domain_mutex); 
-DEFINE_MUTEX(component_mutex);
 struct irq_domain *irq_default_domain;
 
 DEFINE_MUTEX(revmap_trees_mutex);
 LIST_HEAD(irq_domain_list);
 DEFINE_IDR(irq_idr);
 DEFINE_MUTEX(sparse_irq_lock);
-LIST_HEAD(linux_component_list);
-LIST_HEAD(linux_component_masters);
-
 
 struct irq_chip no_irq_chip  = {
 	.name		= "none",
 };
-
