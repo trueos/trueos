e1b62e79b0dc7079ad8172c968bdadae606071ab
Mark Johnston <markjdb@gmail.com>
2017-05-20 18:24:10 -0700

    Remove unused code.

--- sys/compat/linuxkpi/gplv2/include/linux/stop_machine.h
+++ sys/compat/linuxkpi/gplv2/include/linux/stop_machine.h
@@ -2,15 +2,13 @@
 #define _LINUX_STOP_MACHINE
 
 #include <linux/cpu.h>
-#include <linux/cpumask.h>
 #include <linux/smp.h>
 #include <linux/list.h>
 
-struct cpumask;
 typedef int (*cpu_stop_fn_t)(void *arg);
 
 static inline int
-stop_machine(cpu_stop_fn_t fn, void *data, const struct cpumask *cpus)
+stop_machine(cpu_stop_fn_t fn, void *data, void *dummy)
 {
 	int ret;
 	sched_pin();
--- sys/compat/linuxkpi/gplv2/src/linux_stubs.c
+++
@@ -1,7 +0,0 @@
-#include <linux/kernel.h>
-#include <linux/sysfs.h>
-#include <linux/cpumask.h>
-#include <linux/pci.h>
-
-cpumask_t *cpu_all_mask;
-cpumask_t *cpu_online_mask;
