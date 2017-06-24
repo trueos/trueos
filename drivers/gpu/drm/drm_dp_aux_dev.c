398256c0c2eb3cbfb747db52e09678803eca49e0
Mark Johnston <markjdb@gmail.com>
2017-06-24 14:14:36 -0700

    dev_groups isn't implemented.

--- drivers/gpu/drm/drm_dp_aux_dev.c
+++ drivers/gpu/drm/drm_dp_aux_dev.c
@@ -352,15 +352,15 @@ int drm_dp_aux_dev_init(void)
 	if (IS_ERR(drm_dp_aux_dev_class)) {
 		return PTR_ERR(drm_dp_aux_dev_class);
 	}
-	drm_dp_aux_dev_class->dev_groups = drm_dp_aux_groups;
-
-#ifdef __linux__
-	res = register_chrdev(0, "aux", &auxdev_fops);
-#else
+#ifdef __FreeBSD__
 	res = register_chrdev_p(DRM_MAJOR+1, "aux", &auxdev_fops,
 	    DRM_DEV_UID, DRM_DEV_GID, DRM_DEV_MODE);
 	if (res == 0)
 		res = DRM_MAJOR+1;
+#else
+	drm_dp_aux_dev_class->dev_groups = drm_dp_aux_groups;
+
+	res = register_chrdev(0, "aux", &auxdev_fops);
 #endif	
 	if (res < 0)
 		goto out;
