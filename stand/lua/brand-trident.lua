6e19b41dbbbf3072abc988b5d979da4473413ae2
Ken Moore <ken@ixsystems.com>
2018-06-14 13:10:10 -0400

    Oops, forgot to change the copyright in the new trident branding files.

--- stand/lua/brand-trident.lua
+++ stand/lua/brand-trident.lua
@@ -1,7 +1,7 @@
 --
 -- SPDX-License-Identifier: BSD-2-Clause-FreeBSD
 --
--- Copyright (c) 2018 Kris Moore <kmoore@FreeBSD.org>
+-- Copyright (c) 2018 Ken Moore <ken@ixsystems.com>
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions
