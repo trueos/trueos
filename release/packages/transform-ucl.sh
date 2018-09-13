383d13962e6ef7aea4fcb29a18875d36608c2d57
Kris Moore <kris@ixsystems.com>
2018-09-13 00:59:14 -0400

    Add new "transform-ucl.sh" script to runtime/packages/
    We use this to inject our depends for base packages now by adding the
    depend list to TRUEOS_MANIFEST and then inserting these items at
    'make packages' time. This allows us to keep our release/packages/*.ucl
    files in a clean state and only inject in one place.

---
+++ release/packages/transform-ucl.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+LPKG="$1"
+SRCDIR="$2"
+UCL="$3"
+MANIFEST="$4"
+
+if [ ! -e "$MANIFEST" ] ; then
+	echo "Missing MANIFEST"
+	exit 1
+fi
+
+# Verify we have depends in our MANIFEST
+if [ "$(jq -r '."base-packages"."depends"."'$LPKG'"' $MANIFEST)" = "null" ] ; then
+	return 0
+fi
+
+# Add the new depends to the UCL file
+for pkg in $(jq -r '."base-packages"."depends"."'$LPKG'" | keys[]' $MANIFEST)
+do
+	# Get the origin / version for this new depend
+	origin=$(jq -r '."base-packages"."depends"."'$LPKG'"."'$pkg'"."origin"' $MANIFEST)
+	version=$(jq -r '."base-packages"."depends"."'$LPKG'"."'$pkg'"."version"' $MANIFEST)
+
+	# Transform the UCL file and insert the new depend
+	uclcmd get --file $UCL -j '.' | jq -r '."deps" |= .+ { "'$pkg'":{"origin":"'$origin'","version":"'$version'"} }' > ${UCL}.new
+
+	# Convert back to UCL
+	uclcmd get --file ${UCL}.new -u '.' > ${UCL}
+	rm ${UCL}.new
+done
