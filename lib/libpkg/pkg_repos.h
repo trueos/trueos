e0098de4e1ddbc4f7fb0d40cf000b6e897197ffb
Kris Moore <kris@ixsystems.com>
2018-09-22 17:28:07 -0400

    Delete pkg from base, bring in ports-mgmt/pkg as ports

--- lib/libpkg/pkg_repos.h
+++
@@ -1,8 +0,0 @@
-#ifndef PKG_REPOS_H
-#define PKG_REPOS_H
-extern struct pkg_repo_ops pkg_repo_binary_ops;
-struct pkg_repo_ops* repos_ops[] = {
-&pkg_repo_binary_ops,
-NULL
-};
-#endif /* PKG_REPOS_H */
