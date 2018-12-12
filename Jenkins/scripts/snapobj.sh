da1a101f07bc2f7e17ab5df9e607295d6f726afd
Kris Moore <kris@ixsystems.com>
2018-12-12 13:29:50 -0500

    Update FreeNAS PR builds, lets start using ZFS to snapshot and rollback the /usr/obj/usr/freenas-src dataset. This will come into play when we enable full PR builds again and avoid lots of rebuilding of OS/Packages.

---
+++ Jenkins/scripts/snapobj.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# Helper script to take or roll-back a snapshot of /usr/obj/foo
+# build directories so we can do incremental builds on our Jenkins hosts
+
+# Get the default zpool
+ZPOOL=$(mount | grep 'on / ' | cut -d '/' -f 1)
+
+prepare() {
+	mount | grep -q "on $1 "
+	if [ $? -ne 0 ] ; then
+		# Create the dataset
+		zfs create -p -o mountpoint=$1 ${ZPOOL}/$1
+		if [ $? -ne 0 ] ; then
+			exit 1
+		fi
+		# Create the initial snapshot
+		zfs snapshot ${ZPOOL}/${1}@clean
+		if [ $? -ne 0 ] ; then
+			exit 1
+		fi
+	fi
+}
+
+snapshot() {
+	# Cleanup old snapshot (if it exists)
+	zfs destroy ${ZPOOL}/${1}@clean
+
+	# Create the snapshot
+	zfs snapshot ${ZPOOL}/${1}@clean
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+}
+
+rollback() {
+	# Rollback to pristine state snapshot
+	zfs rollback ${ZPOOL}/${1}@clean
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+}
+
+# Make sure datasets are all in place
+prepare
+
+case $1 in
+	snapshot) snapshot $2 ;;
+	rollback) rollback $2 ;;
+	*) echo "Invalid option specified: $1"
+		exit 1
+		;;
+esac
+
